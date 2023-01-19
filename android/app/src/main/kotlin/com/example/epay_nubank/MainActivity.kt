82a44ae8a0d48881f37447a7059ff689858476a8
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2023-01-19 19:48:11 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/epay_nubank/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.epay_nubank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:epay_nubank/WebViewExample.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            child: const Text('Open WebView'),
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const WebViewExample()),
+              )
+            }),
+      ),
+    );
+  }
+}
